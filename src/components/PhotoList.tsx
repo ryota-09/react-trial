634fa6a3fed8bcd385a5ce70dd55872d7aee996e
ryota-09 <n.ryo950911@gmail.com>
2024-03-23 11:27:56 +0900

    遅い環境を作成

--- src/components/PhotoList.tsx
+++ src/components/PhotoList.tsx
@@ -8,18 +8,30 @@ export const PhotoList = () => {
   useEffect(() => {
     const getphotos = async () => {
       setIsLoading(true)
-      const photos = await Promise.all(Array(10).fill(fetch('https://jsonplaceholder.typicode.com/photos').then((res) => res.json())));
+      const photos = await Promise.all(Array(5).fill(fetch('https://jsonplaceholder.typicode.com/photos').then((res) => res.json())));
       setPhoto(photos.flat());
       setIsLoading(false)
     }
     getphotos()
   }, [])
+
+  // useEffect(() => {
+  //   setPhoto(photos.map((photo: any) => ({ ...photo, title: photo.title.toUpperCase() })))
+  // }, [photos])
+
   return (
     <>
-      <h1>Photos</h1>
-      {isloading && <div>Loading...</div>}
-      <div>
-        {photos.map((photo: any, index: any) => <div key={index}>{photo.title}</div>)}
+      <div className="w-[768px] mx-auto">
+        <h1 className="bg-red-500">Photos</h1>
+        {isloading && <div>Loading...</div>}
+        <ul className="">
+          {photos.map((photo: any, index: any) => (
+            <li key={index} className="cursor-pointer h-10 hover:bg-slate-300 flex gap-4">
+              <img src={photo.url} width={20} height={20} />
+              <p>{photo.title}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   )
